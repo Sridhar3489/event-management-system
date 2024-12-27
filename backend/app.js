ae0331edfa0901861981175c4930019cd0ee0ae6
Sridhar3489 <saisridharparimi@gmail.com>
2024-12-27 12:25:57 -0500

    Backend changes

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ const eventRoutes = require('./routes/events');  // Import event routes
 const {registerRouter} = require('./routes/register')
 const {roleRouter} = require('./routes/role')
 const {loginRouter} = require('./routes/login')
+const {userRouter} = require('./routes/user')
 const app = express();
+const cors = require("cors")
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors())
 
 // Connect to MongoDB
 connectDB();
@@ -17,6 +20,7 @@ app.use('/api/signup', registerRouter)
 app.use('/api/events', eventRoutes);  // Add the event routes
 app.use('/api/roles', roleRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/user', userRouter)
 
 // Start the server
 const PORT = process.env.PORT || 3000;
